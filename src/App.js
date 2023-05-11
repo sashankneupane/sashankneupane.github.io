2cf9372b40784577a4002eb11eff8f179638613f
sashankneupane7 <sn3006@nyu.edu>
2023-05-11 22:04:15 +0200

    added _redirects for netlify and 404 page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 import CV from './pages/CV';
+import NotFound from './components/NotFound';
 
 export default function App() {
   return (
@@ -9,6 +10,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/cv" />} />
         <Route path="/cv" element={<CV />} />
+        <Route path="*" element={<Navigate to="/404" />} />
+        <Route path="/404" element={<NotFound />} />
       </Routes>
     </Router>
   );
---
+++ src/components/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/404.css';
+
+const NotFound = () => {
+    return (
+        <div className = "main404">
+            <h1>404 Not Found</h1>
+            <a href="/">Go back to the homepage</a>
+        </div>
+    );
+}
+
+export default NotFound;
+
