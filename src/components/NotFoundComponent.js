7cda74fe854ac5ea18d7dabecf1ba128a38cc630
sashankneupane7 <sn3006@nyu.edu>
2023-05-12 00:37:01 +0200

    fixed 404 eror infinite reloading

--- src/components/NotFoundComponent.js
+++ src/components/NotFoundComponent.js
@@ -4,7 +4,10 @@ import '../styles/404.css';
 const NotFoundComponent = () => {
     return (
         <div>
-            <NotFoundComponent />
+            <div className = "main404">
+            <h1>404 Not Found</h1>
+            <a href="/">Go back to the homepage</a>
+        </div>
         </div>
     );
 }
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -5,8 +5,9 @@ import NotFoundComponent from '../components/NotFoundComponent';
 
 const NotFound = () => {
     return (
+
         <div>
-            <Helmet><title>404 Not Found</title></Helmet>
+            <Helmet><title>404</title></Helmet>
             <NotFoundComponent />
         </div>
     );
